0e6bc8fc693e1df030134b71af4730e98e1a1f9c
Alexander Stalev <alexander.stalev@gmail.com>
2016-02-12 13:06:50 +0300

    Черновая реализация. 1-й тест проходит.

--- src/normalizePath/normalizePath.js
+++ src/normalizePath/normalizePath.js
@@ -1,3 +1,53 @@
 function normalizePath(path) {
-    
+    /**
+     * Возвращает массив с частями пути.
+     */
+    function getParts(path) {
+        var parts = [];
+        var startIndex;
+
+        for (var i = 0; i < path.length; i++) {
+            if (i === 0 && startIndex === 0 && path[0] === '/') {
+                startIndex = 1;
+                continue;
+            }
+
+            if (path[i] !== '/') {
+                continue;
+            } else {
+                var part = path.substring(startIndex, i);
+                parts.push(part);
+                startIndex = i + 1;
+            }
+        }
+
+        if (startIndex < path.length) {
+            var part = path.substring(startIndex, path.length);
+            parts.push(part);
+        }
+
+        return parts;
+    }
+
+    var parts = getParts(path);
+    var resultPath = '';
+
+    while (parts.length) {
+        var part = parts.pop();
+
+        var skip = false;
+        while (part === '..') {
+            part = parts.pop();
+            skip = true;
+        }
+        if (skip) {
+            continue;
+        }
+
+        resultPath = resultPath 
+                    ? part + '/' + resultPath
+                    : part;
+    }
+
+    return resultPath;
 }
