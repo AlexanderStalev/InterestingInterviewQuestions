dc01c23f3f6acbc28529d3ccfd73f260b22c3e55
Alexander Stalev <alexander.stalev@gmail.com>
2016-03-02 00:52:54 +0300

    Еще тесты.

--- src/seeFight/seeFight.unit.js
+++ src/seeFight/seeFight.unit.js
@@ -36,6 +36,46 @@ describe('seeFight.', function () {
         expect(count).toBe(1);
     });
     
+     it('1 корабль на поле 2x2 (в правом нижнем углу).', function () {
+        // Arrange.
+        var battlefield = [
+            0,0,
+            0,1    
+        ];
+        // Act!
+        var count = shipCount(battlefield);
+        
+        // Assert.    
+        expect(count).toBe(1);
+    });
+    
+    it('2 корабля на поле 2x2.', function () {
+        // Arrange.
+        var battlefield = [
+            1,0,
+            0,1    
+        ];
+        // Act!
+        var count = shipCount(battlefield);
+        
+        // Assert.    
+        expect(count).toBe(2);
+    });
+    
+     it('2 корабля на поле 2x2.', function () {
+        // Arrange.
+        var battlefield = [
+            0,1,
+            1,0    
+        ];
+        // Act!
+        var count = shipCount(battlefield);
+        
+        // Assert.    
+        expect(count).toBe(2);
+    });
+    
+    
     it('3 корабля.', function () {
         // Arrange.
         var battlefield = [
